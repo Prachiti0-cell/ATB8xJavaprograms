cf1bfbf9cec2bf4be5d61bb0f2635096dab75515
prachiti <revtithombare871@gmail.com>
2024-09-26 13:18:05 -0700

    Lab004

---
+++ src/Sept/ex_06092024/Lab004.java
@@ -0,0 +1,11 @@
+package Sept.ex_06092024;
+
+public class Lab004 {
+    //main is the entry point
+    public static void main(String[]args){
+        //Below is the code to show the output
+        System.out.println("Hello world!");
+        System.out.println(8+8);
+    }
+
+}
